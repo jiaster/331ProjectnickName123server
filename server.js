9958d8e8a8093b7c4f8dec7ea0c5d832c56add7a
Jia Wei Zheng <jiasterop@gmail.com>
2018-10-30 00:01:52 -0400

    websocket starting point

--- server.js
+++ server.js
@@ -33,7 +33,16 @@ wss.on('connection', function connection(ws) {
     console.log('Client connected');
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-        ws.send(message);
+        //ws.send(message);
+        wss.broadcast = function broadcast(data) {
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        };
+
+
     });
 
     wss.on('close', () => console.log('Client disconnected'));
