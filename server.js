77e6b3e8485d46ca606f02a4a3fe4c9cfe362d7a
Jia Wei Zheng <jiawei.zheng@stonybrook.edu>
2018-11-27 19:38:57 -0500

    test

--- server.js
+++ server.js
@@ -290,7 +290,7 @@ wss.on('connection', function connection(ws) {//Upon a connection from a client
     }
 
     wss.clients.forEach(function each(client) {//sends message back to ALL clients MUST CHANGE
-        client.log(client);
+        console.log(client);
         if (client.readyState === WebSocket.OPEN) {
             client.send(message);
             console.log(message+ " sent to "+client.id);
