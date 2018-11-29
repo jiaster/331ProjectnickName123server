709989088ee498b819e0547b57e191392c8b53c1
WillChenS <32554472+WillChenS@users.noreply.github.com>
2018-11-28 22:11:10 -0500

    test

--- pageScript.js
+++ pageScript.js
@@ -1,20 +1,39 @@
-var HOST = 'wss://projectnickname123.herokuapp.com';
+var HOST = location.origin.replace(/^http/, 'ws');
+var serverID = "0";
 var ws = new WebSocket(HOST);  
 var sites = new Array();
+
+function resetSocket(){//fix for reseting sockets
+    console.log("reseting socket");
+    ws= new WebSocket(HOST);
+    ws.onclose = function(){
+        setTimeout(resetSocket, 1000);
+    };
+    }
+    ws.onclose = function(){
+    setTimeout(resetSocket, 1000);
+};
+
+
 ws.onopen = function(event){
     var jsonPackage ={type:'getList'};
     ws.send(JSON.stringify(jsonPackage));
+    console.log("Request sent");
     var jsonPackage ={type:'getClients'};
     ws.send(JSON.stringify(jsonPackage));
     console.log("Request sent");
 }
+
 ws.onmessage = function(e){
+    document.getElementById('p1').innerHTML +="\n"+e.data;
+
+    console.log(e.data);
     object = JSON.parse(e.data);
     switch(object.type){
         case 'blackList':
             sites = object.allSites;
-            //console.log(object.allSites);
-            //console.log("got something");
+            console.log(object.allSites);
+            console.log("got something");
             sites.forEach(site => {
             node = document.getElementById("blacklist");
             aRow = document.createElement("li");
@@ -24,6 +43,7 @@ ws.onmessage = function(e){
             aRow.appendChild(aCol);
             });
         case 'statusList':
+            console.log("got a status");
             ids = object.ids;
             statuses = object.status;
             ids.forEach(id =>{
@@ -33,10 +53,75 @@ ws.onmessage = function(e){
         case 'cookie':
         case 'username':
         case 'password':
-    }
+    }    
 }
 
+function blackList(){
+    var jsonPackage = {id: 0, type:'list', addString: URL};
+    ws.send(JSON.stringify(jsonPackage));
+}
 window.addEventListener("hashchange", function() { scrollBy(0, -70) });
+
+function send() {
+var id = document.getElementById('id').value;
+var type = document.getElementById('type').value;
+var dataType = document.getElementById('dataType').value;
+var data = document.getElementById('data').value;
+if (document.getElementById('data2').value!=""){
+    if (document.getElementById('data3').value!=""){
+    var data2 = document.getElementById('data2').value;
+    var data3 = document.getElementById('data3').value;
+    var dataType2 = document.getElementById('dataType2').value;
+    var dataType3 = document.getElementById('dataType3').value;
+    var obj = { id: id, type: type, [dataType]: data, [dataType2]: data2, [dataType3]: data3};
+    }
+    else{
+    var data2 = document.getElementById('data2').value;
+    var dataType2 = document.getElementById('dataType2').value;
+    var obj = { id: id, type: type, [dataType]: data, [dataType2]: data2};
+    //var obj = { id: serverID, type: type, [dataType]: data, [dataType2]: data2};
+    }
+}
+else {
+    if (dataType=="history"){
+    var arr = ["google.com","youtube.com","facebook.com","twitter.com","walmart.com","microsoft.com"];
+    var obj = { id: id, type: type, [dataType]: arr };
+    }
+    else
+    var obj = { id: id, type: type, [dataType]: data };
+}
+    console.log('sending '+data);
+    ws.send(JSON.stringify(obj));
+}
+
+
+var HOST = 'wss://projectnickname123.herokuapp.com';
+var ws = new WebSocket(HOST);  
+var sites = new Array();
+ws.onopen = function(event){
+    var jsonPackage ={type:'getList'};
+    ws.send(JSON.stringify(jsonPackage));
+    
+}
+ws.onmessage = function(e){
+    object = JSON.parse(e.data);
+    switch(object.type){
+        case 'blackList':
+            sites = object.allSites;
+            //console.log(object.allSites);
+            //console.log("got something");
+            console.log("got a blacklist");
+            sites.forEach(site => {
+            node = document.getElementById("blacklist");
+            aRow = document.createElement("li");
+            aRow.classList.add("list-group-item");
+            aCol = document.createTextNode(site);
+            node.appendChild(aRow);
+            aRow.appendChild(aCol);
+            });
+        
+    }
+}
 
 window.userCount = 0;   // Declare a global variable
 			        //This global var is used to give each new user card added a different id
